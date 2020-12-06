47ff6656fa1183e1fa1a237e8b823f32861f8a21
ratnesh singh <dev.ratnesh.singh@gmail.com>
2020-12-06 21:09:33 +0530

    basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Learn';
+  title = 'Welcome';
+
+  className = "textRed";
+
+  show() {
+
+  }
+
+  changeClass( event ) {
+    console.log( event );
+    this.className = "textGreen";
+  }
 }
